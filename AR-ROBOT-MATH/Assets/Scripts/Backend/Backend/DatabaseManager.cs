0cd3bbbbec7043632689e67ab377741230c91812
paigeharvey <paigeharvey02@gmail.com>
2024-02-01 13:08:02 -0500

    Setup-Creating initial files

---
+++ AR-ROBOT-MATH/Assets/Scripts/Backend/Backend/DatabaseManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Backend
+{
+	public class DatabaseManager
+	{
+		public DatabaseManager()
+		{
+		}
+	}
+}
+
---
+++ AR-ROBOT-MATH/Assets/Scripts/Backend/Backend/MathBackend.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Backend
+{
+	public class MathBackend
+	{
+		public MathBackend()
+		{
+		}
+	}
+}
+
