6cb0bcb888b5b8bbae230e469b79bc5f70fa7561
anngedeus <76603380+anngedeus@users.noreply.github.com>
2024-02-20 12:53:36 -0500

    seperate disks script

---
+++ AR-ROBOT-MATH/Assets/Scripts/RotateDiskT.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class RotateDiskT : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 oldTouchPosition;
+    private Vector2 NewTouchPosition;
+    [SerializeField]
+    private float keepRotateSpeed = 10f;
+
+    private void Update()
+    {
+        RotateThings();
+    }
+    private void RotateThings()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                oldTouchPosition = touch.position;
+            }
+
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                NewTouchPosition = touch.position;
+            }
+
+            Vector2 rotDirection = oldTouchPosition - NewTouchPosition;
+            Debug.Log(rotDirection);
+            if (rotDirection.x < 0)
+            {
+                RotateRight();
+            }
+            else if (rotDirection.x > 0)
+            {
+                RotateLeft();
+            }
+        }
+    }
+
+    void RotateLeft()
+    {
+        transform.rotation = Quaternion.Euler(0f, 1.5f * keepRotateSpeed, 0f) * transform.rotation;
+    }
+
+    void RotateRight()
+    {
+        transform.rotation = Quaternion.Euler(0f, -1.5f * keepRotateSpeed, 0f) * transform.rotation;
+    }
+
+}
