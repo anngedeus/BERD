ac81c672635ec27d5be3d3feb5ee99f967a76c40
paigeharvey <paigeharvey02@gmail.com>
2024-02-20 22:14:22 -0500

    Updated backend api to use remote azure resources, no more local host!

--- AR-ROBOT-MATH/Assets/Scripts/DisplayScript.cs
+++ AR-ROBOT-MATH/Assets/Scripts/DisplayScript.cs
@@ -6,23 +6,32 @@ using UnityEngine.UI;
 public class DisplayScript : MonoBehaviour
 {
     public Text displayText;
-    //public string newText = "New Text String";
+    public Dictionary<string, string> mathQuestion = new Dictionary<string, string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        //BackendApi backendApiEndpoint = new BackendApi();
-        //Dictionary<string, string> mathQuestion = backendApiEndpoint.GetMathProblem();
-
+        // BackendApi backendApiEndpoint = new BackendApi();
+        // mathQuestion = backendApiEndpoint.getMathQuestion(); // Assign the value returned by getMathQuestion to mathQuestion
+        // Debug.Log(mathQuestion);
     }
+
     
     void ChangeText()
     {
-        //displayText.text = mathQuestion["Question"];
+        // if (mathQuestion != null && mathQuestion.ContainsKey("question"))
+        // {
+        //     displayText.text = mathQuestion["question"];
+        // }
+        // else
+        // {
+        //     displayText.text = "No question available";
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChangeText();
+        // ChangeText();
     }
 }
