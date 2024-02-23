58f1901d16cae22ea598127a801d69bf17ad70f4
nabilkoneylaryea <nkoney14@outlook.com>
2024-02-23 13:21:30 -0500

    It works used port forwarding!

--- NAO/nao.py
+++ NAO/nao.py
@@ -1,23 +1,31 @@
 import socket
 from naoqi import ALProxy
 
-tts = ALProxy("ALTextToSpeech", "10.0.1.37", 9559)
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
 
-host, port = "", 25000
+tts = ALProxy("ALTextToSpeech", "10.0.1.37", 9559)
+tts.say("Starting application on remote machine.")
+
+host, port = "", 25001
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
+
+tts.say("Listening for connections")
 print 'Listening ...'
+
 sock.listen(1)
 conn, addr = sock.accept()
 
-
+tts.say("Connected to Unity application.")
 print 'Connected by', addr
 		
 while True:
 	data = conn.recv(1024)
 	if not data:
 		break
+	tts.say("Data is " + data)
 	print 'Getting data: ', data
 	tts.say(data)
 	conn.sendall(data)
