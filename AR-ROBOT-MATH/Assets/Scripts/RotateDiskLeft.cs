7e0e03032256c93b97ae13989958c1fc55c74dc7
anngedeus <76603380+anngedeus@users.noreply.github.com>
2024-02-22 21:03:46 -0500

    Revert "Merge branch 'dev-branch' into anngedeus"
    This reverts commit e80899bc7d3a1dce97be5476c12dc8c7973a1056, reversing
    changes made to 29113e5a5a091ebda9f9f3702b5d85b33e9bb578.

--- AR-ROBOT-MATH/Assets/Scripts/RotateDiskLeft.cs
+++ AR-ROBOT-MATH/Assets/Scripts/RotateDiskLeft.cs
@@ -4,12 +4,13 @@ using TMPro;
 public class RotateDiskLeft : MonoBehaviour
 {
     private Touch touch;
-    private float oldTouchPosition;
+    private Vector3 oldTouchPosition;
+    private Vector3 NewTouchPosition;
     public TMP_Text leftText;
     private int randomNumber;
-    //[SerializeField]
-    //private float rotationAngle;
-    private bool isDragging = false;
+    [SerializeField]
+    private float keepRotateSpeed = 10f;
+    private float rotationAngle;
 
     private void Start()
     {
@@ -26,7 +27,6 @@ public class RotateDiskLeft : MonoBehaviour
     {
         RotateThings();
     }
-
     private void RotateThings()
     {
         if (Input.touchCount > 0)
@@ -34,48 +34,44 @@ public class RotateDiskLeft : MonoBehaviour
             touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                oldTouchPosition = touch.position.x;
-                isDragging = true;
+                oldTouchPosition = touch.position;
             }
 
             else if (touch.phase == TouchPhase.Moved)
             {
-                if (isDragging)
-                {
-                    float swipeValue = touch.position.x - oldTouchPosition;
-                    if (swipeValue < 0)
-                    {
-                        //Disk will rotate right
-                        //transform.Rotate(0f, 0f, -2f, Space.World);
-                        transform.Rotate(Vector3.forward, swipeValue * 1.5f, Space.World);
-                        randomNumber++;
-                        if (randomNumber > 12)
-                        {
-                            randomNumber = 2;
-                        }
-                        leftText.text = randomNumber.ToString();
-                    }
-                    else if (swipeValue > 0)
-                    {
-                        //Disk will rotate left
-                        //transform.Rotate(0f, 0f, 2f, Space.World);
-                        transform.Rotate(Vector3.forward, swipeValue * 1.5f, Space.World);
-                        randomNumber--;
-                        if (randomNumber < 2)
-                        {
-                            randomNumber = 12;
-                        }
-                        leftText.text = randomNumber.ToString();
-                    }
-
-                    oldTouchPosition = touch.position.x;
-                }
+                NewTouchPosition = touch.position;
+                rotationAngle = (NewTouchPosition.z - oldTouchPosition.z) * keepRotateSpeed;
+                transform.Rotate(Vector3.up, rotationAngle, Space.World);
+                oldTouchPosition = NewTouchPosition;
             }
 
-            else if (touch.phase == TouchPhase.Moved)
+            Vector3 rotDirection = oldTouchPosition - NewTouchPosition;
+            Debug.Log(rotDirection);
+            if (rotDirection.z < 0)
             {
-                isDragging = false;
+                RotateRight();
+            }
+            else if (rotDirection.z > 0)
+            {
+                RotateLeft();
             }
         }
     }
+
+    void RotateLeft()
+    {
+        transform.rotation = Quaternion.Euler(0f, 1.5f * keepRotateSpeed, 0f) * transform.rotation;
+        randomNumber--;
+        leftText.text = randomNumber.ToString();
+        Debug.Log("I'm decreasing");
+    }
+
+    void RotateRight()
+    {
+        transform.rotation = Quaternion.Euler(0f, -1.5f * keepRotateSpeed, 0f) * transform.rotation;
+        randomNumber++;
+        leftText.text = randomNumber.ToString();
+        Debug.Log("I'm increasing");
+    }
+
 }
