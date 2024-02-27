4f1d27a38850fde8bf358b96809dc42f905e029a
nehemieyac <74078487+nehemieyac@users.noreply.github.com>
2024-02-26 21:34:07 -0500

    Right Disk and Audio Change Functionality

--- AR-ROBOT-MATH/Assets/Scripts/SubmitButton.cs
+++ AR-ROBOT-MATH/Assets/Scripts/SubmitButton.cs
@@ -12,7 +12,14 @@ public class SubmitButton : MonoBehaviour
     public BackendApi backendApiEndpoint;
     public GameObject backendApiObject;
     public GameObject BGColor;
+    public int stemNumber = 0;
+    public string currentSong = "Beat 1 - Peaches & Eggplants";
+    public AudioSource audioSource;
 
+    private void Start()
+    {
+        ChangeStem();
+    }
 
     public void ButtonPressed()
     {
@@ -22,12 +29,12 @@ public class SubmitButton : MonoBehaviour
         backendApiEndpoint.validateAnswer(leftNumber, rightNumber);
         ChangeTextDisplay();
         RandomGenerator();
-      
+        ChangeStem();
     }
-   
+
     private void ChangeTextDisplay()
     {
-       
+
         string nextDifficultyLevel = backendApiEndpoint.DetermineNextDifficultyLevel();
         backendApiEndpoint.RequestNewQuestion(nextDifficultyLevel);
         BGColor.GetComponent<TextMeshProUGUI>().text = backendApiEndpoint.mathQuestion["question"];
@@ -39,5 +46,28 @@ public class SubmitButton : MonoBehaviour
         randomNumber = Random.Range(2, 13);
         leftText.text = randomNumber.ToString();
         rightText.text = randomNumber.ToString();
+    }
+
+    private void ChangeStem()
+    {
+        stemNumber++;
+        string audioFilePath = "../Beats/Beat Stems/Beat Stems (Mashed)/" + currentSong + "/Part " + stemNumber;
+        // Load the audio clip from the specified file path
+        AudioClip audioClip = Resources.Load<AudioClip>(audioFilePath);
+
+        if (audioClip != null)
+        {
+            // Change the audio clip to the loaded one
+            audioSource.clip = audioClip;
+
+            // Play the audio
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Failed to load audio clip: " + audioFilePath);
+            stemNumber = 0;
+            currentSong = "Beat 2 - Turks & Caicos";
+        }
     }
 }
